1e6c0f30f947f8530c7e42496bfa6253ffe1aac7
shaelito <shaelito@gmail.com>
2018-12-12 13:11:36 -0500

    publicando mysql

--- config/conn.js
+++ config/conn.js
@@ -1,17 +1,17 @@
 Sequelize = require('sequelize');
-// var conn_db = {
-//    db_name: "caol1",
-//    user: "root",
-//    password: "root",
-//    host: "localhost",
-// }
+ var conn_db = {
+    db_name: "sql10269761",
+    user: "sql10269761",
+    password: "wTSBxs324e",
+    host: "sql10.freemysqlhosting.net",
+ }
 
-var conn_db = {
+/*var conn_db = {
     db_name: "ladatwoo_cao",
     user: "ladatwoo_shael",
     password: "shaelcao",
     host: "67.20.76.109",
-}
+}*/
 
 var sequelize = new Sequelize(conn_db.db_name, conn_db.user, conn_db.password, {
     host: conn_db.host,
